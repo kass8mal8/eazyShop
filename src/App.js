8d888e3f5064b3c651ec152c23ef6d8574f7baaa
kassimaly21@gmail.com <kassimaly21@gmail.com>
2022-06-13 10:39:37 +0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import {
+   BrowserRouter, 
+   Routes, 
+   Route}
+from 'react-router-dom'
+import Header from './Header'
+import Mainproducts from "./Mainproducts"
+import About from './About'
+import Home from './Home'
+import Signup from './Signup' 
+import { auth } from './firebase'
+import
+{ onAuthStateChanged } from 'firebase/auth'
 
-export default function App() {
+const App =()=>{
+  
+
+  const handleCart=()=>{
+     alert('hello world')
+     document.querySelector('.cart-content ').classList.toggle('show') 
+  }
+  
+
+  const [count, setCount]=useState(0) 
+  
+  /*checking if user is currently logged in*/
+  const [user, setUser] =useState({})
+  onAuthStateChanged(auth, (currentUser)=>{
+      setUser(currentUser)
+  })
+  
+  
+ 
+  const src= "https://pluspng.com/img-png/clothes-png-hd-dress-shirt-png-hd-png-image-480.png"
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <BrowserRouter>
+        <Header
+            handleCart={handleCart}
+            user={user} 
+            count={count} />
+                  
+         <div className="cart-content">
+            <div 
+            style={{
+               borderBottom:'2px solid #ccc',
+               fontWeight:'bold'
+            }}>
+                <p>Cart</p>
+            </div>
+         {count < 1 ?
+         <p style={{
+            color:'gray',
+            textAlign:'center',
+            marginTop:'22%'}}>
+            You have no items in your cart
+         </p> :
+         <div>
+            <img src={src} width='100' />
+            <button 
+            style={{
+              background:'hsl(25,100%,60%)',
+              border:'1px solid hsl(25,100%,60%)',
+              width:'100%',
+              marginTop:'15%',
+              padding:'10px',
+              fontFamily:'kumbh sans',
+              borderRadius:'2px'
+            }}>
+              proceed to checkout
+            </button>
+         </div>}
+         </div>
+ 
+          
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route 
+            path="/products" 
+            element={
+               <Mainproducts 
+                  count={count} 
+                  setCount={setCount}/>
+               }/>
+          <Route
+            path="/signup" 
+            element={
+               <Signup 
+                  user={user} />
+            }/>
+        </Routes>
+        
+      </BrowserRouter>
     </div>
   );
 }
+export default App 
