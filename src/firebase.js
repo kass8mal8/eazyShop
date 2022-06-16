55d40afa3c479d2a022b33ddfd15917a359b1fac
Kassim Ali <105120332+kass8mal8@users.noreply.github.com>
2022-06-16 11:49:57 +0300

    update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,18 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCaYEfbQHLmdV_lHwka5IzWh08dDYmn4hE",
+  authDomain: "eazy-shop-157bf.firebaseapp.com",
+  projectId: "eazy-shop-157bf",
+  storageBucket: "eazy-shop-157bf.appspot.com",
+  messagingSenderId: "198553110238",
+  appId: "1:198553110238:web:6db880a8d2aa32e3f6e44f"
+};
 
-   // Your web app's Firebase configuration
-   const firebaseConfig = {
-     apiKey: "AIzaSyCIMC-BGvBeO4VUg7DbPJq-F0Z_z87LyAM",
-     authDomain: "eazy-mart-auth.firebaseapp.com",
-     projectId: "eazy-mart-auth",
-     storageBucket: "eazy-mart-auth.appspot.com",
-     messagingSenderId: "749499150162",
-     appId: "1:749499150162:web:f49130694ac44b3c40948b"
-   };
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
